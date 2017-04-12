7fe2a72cbe35f5b9faa2730df99521bd1f2a0401
menonmeg <menonmeg@umich.edu>
2017-04-11 20:49:06 -0400

    Demo ready

--- drawBotControl.py
+++ drawBotControl.py
@@ -25,7 +25,10 @@ WAIT_DUR = 0.2
 TURN_TORQUE = 0.2
 TURN_DUR = 0.2
 
-X_TORQUE = 5.0
+X_TORQUE = 0.95
+X_DUR = 0.5
+
+SERVO_POS = 500
 
 class MovePlan( Plan ):
     """
@@ -48,18 +51,11 @@ class MovePlan( Plan ):
         # due to orientation of motors
         self.r.lwheel.set_torque(-1*self.torque)
         self.r.rwheel.set_torque(self.torque)
-	self.r.xmotor.set_torque(0)
-	self.r.lservo.set_torque(0)
-	self.r.rservo.set_torque(0)
 
         yield self.forDuration(self.dur)
 
 	self.r.lwheel.set_torque(0)
         self.r.rwheel.set_torque(0)
-	self.r.xmotor.set_torque(0)
-	self.r.lservo.set_torque(0)
-	self.r.rservo.set_torque(0)
-
 
 class RotatePlan( Plan ):
     """
@@ -108,27 +104,76 @@ class XPlan( Plan ):
     def __init__(self,app):
         Plan.__init__(self,app)
         self.r = self.app.robot.at
-        self.xSpeed = X_TORQUE
+        self.xtorque = X_TORQUE
+	self.dur = X_DUR
 
     def behavior(self):
-        # Set timeout stamp
-        timeout = time.time() + 5.0
-
+       
         # Set torque to adjust turret
-        self.r.xmotor.set_torque(self.xSpeed)        
+        self.r.xmotor.set_torque(self.xtorque)        
+	
+	yield self.forDuration(self.dur)
 
-        # Set torque to 0 after time duration to stop moving
-        while True:
-            if time.time() > timeout:
-                self.r.xmotor.set_torque(0)
-                break
-     	yield
+	self.r.xmotor.set_torque(self.xtorque)
+
+
+class ServoPlan( Plan ):
+    """
+    Plan that moves buggy forward or backward by setting equal torque on wheels
+    for a fixed duration of time.
+
+    Torque and Duration are public variables that can be modified on the fly.
+    To move buggy backwards set torque to a negative value.
+    """
+
+    def __init__(self,app):
+	Plan.__init__(self,app)
+        self.r = self.app.robot.at
+        self.pos = SERVO_POS
+        self.dur = MOVE_DUR
+
+
+    def behavior(self):
+        # Set torque to move wheels. Right wheel must be opposite of left wheel
+        # due to orientation of motors
+
+	self.r.lservo.set_pos(self.r.lservo.get_pos() + (-1*self.pos))
+        self.r.rservo.set_pos(self.r.rservo.get_pos() + self.pos)
+
+	yield self.forDuration(self.dur)
+
+class StopPlan( Plan ):
+    """
+    Plan that moves buggy forward or backward by setting equal torque on wheels
+    for a fixed duration of time.
+
+    Torque and Duration are public variables that can be modified on the fly.
+    To move buggy backwards set torque to a negative value.
+    """
+
+    def __init__(self,app):
+	Plan.__init__(self,app)
+        self.r = self.app.robot.at
+        self.torque = SERVO_POS
+        self.dur = MOVE_DUR
+
+
+    def behavior(self):
+        # Set torque to move wheels. Right wheel must be opposite of left wheel
+        # due to orientation of motors
+
+	self.r.xmotor.set_torque(0)
+
+	yield self.forDuration(self.dur)
+
+
 
 class DrawingBotApp( JoyApp ):
+	
 
     def __init__(self, *arg,**kw):
         JoyApp.__init__( self,
-          confPath="$/cfg/JoyApp.yml", *arg, **kw
+          confPath="$/cfg/JoyApp.yml",*arg, **kw 
           )
 
     def onStart(self):
@@ -136,52 +181,56 @@ class DrawingBotApp( JoyApp ):
         self.moveP = MovePlan(self)
         self.turnP = RotatePlan(self)
 	self.xP = XPlan(self)
+	self.servoP = ServoPlan(self)
+	self.stopP = StopPlan(self)
 
     def onEvent(self, evt):
         if evt.type != KEYDOWN:
                 return
 
         if evt.type == KEYDOWN:
-            if evt.key == K_UP and not self.moveP.isRunning():
+            if evt.key == K_UP:
                 # Forward plan
                 self.moveP.torque = -1 * MOVE_TORQUE
                 self.moveP.start()
                 return progress("(say) Move forward")
-            elif evt.key == K_DOWN and not self.moveP.isRunning():
+            elif evt.key == K_DOWN:
                 # Backward plan
                 self.moveP.torque = MOVE_TORQUE
                 self.moveP.start()
                 return progress("(say) Move back")
-            elif evt.key == K_LEFT and not self.turnP.isRunning():
+            elif evt.key == K_LEFT:
                 # Turn left plan
                 self.turnP.torque = -1 * TURN_TORQUE
                 self.turnP.start()
                 return progress("(say) Turn left")
-            elif evt.key == K_RIGHT and not self.turnP.isRunning():
+            elif evt.key == K_RIGHT:
                 # Turn right plan
                 self.turnP.torque =  TURN_TORQUE
                 self.turnP.start()
                 return progress("(say) Turn right")
-            elif evt.key == K_m and not (self.turnP.isRunning() or  self.moveP.isRunning()):
-                self.xP.turretSpeed = X_TORQUE 
+            elif evt.key == K_m:
+                self.xP.xtorque = X_TORQUE 
                 self.xP.start()
                 return progress("(say) adjusted x")
-            elif evt.key == K_n and not (self.turnP.isRunning() or  self.moveP.isRunning()):
-                self.xP.turretSpeed = -1*X_TORQUE 
+            elif evt.key == K_n:
+                self.xP.xtorque = -1 * X_TORQUE 
                 self.xP.start()
                 return progress("(say) adjusted x")
+	    elif evt.key == K_z:
+		self.servoP.pos = SERVO_POS
+		self.servoP.start()
+	    elif evt.key == K_x:
+		self.servoP.pos = -1*SERVO_POS
+		self.servoP.start()
+	    elif evt.key == K_SPACE:
+		self.stopP = 0
+		self.stopP.start()
 	
    
 
 #runs on main
 if __name__=="__main__":
-    print """
-    Running the robot simulator
-
-    Listens on local port 0xBAA (2986) for incoming waypointServer
-    information, and also transmits simulated tagStreamer messages to
-    the waypointServer.
-    """
     import sys
     app=DrawingBotApp(robot = dict(count = 5))
     app.run()
